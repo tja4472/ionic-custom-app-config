7b15032aed43c4f3fdc81184113072f7613f192c
Tim <tja2353@gmail.com>
2017-12-24 10:11:28 +0000

    add webpack-bundle-analyzer

--- tools/config/ionic.webpack.js
+++ tools/config/ionic.webpack.js
@@ -1,1 +1,22 @@
-console.log('** custom ionic.webpack.js **');
+console.log("** custom ionic.webpack.js **");
+
+var ionicConfig = require("@ionic/app-scripts/config/webpack.config.js");
+var BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+
+// dev config
+ionicConfig.dev.plugins.push(new BundleAnalyzerPlugin());
+
+//prod config
+ionicConfig.prod.plugins.push(new BundleAnalyzerPlugin());
+
+var ModuleConcatPlugin = require("webpack/lib/optimize/ModuleConcatenationPlugin");
+// Remove ModuleConcatPlugin
+ionicConfig.prod.plugins = ionicConfig.prod.plugins.filter(plugin => {
+  return !(plugin instanceof ModuleConcatPlugin);
+});
+
+module.exports = {
+  dev: ionicConfig.dev,
+  prod: ionicConfig.prod
+};
